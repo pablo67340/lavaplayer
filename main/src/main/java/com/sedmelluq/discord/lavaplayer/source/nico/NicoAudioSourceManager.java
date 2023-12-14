cb74858061624bb92b89901c37d2bb7461c4f58d
Devoxin <lukel9914@gmail.com>
2023-12-14 00:00:00 +0000

    Fix NicoNico source manager.

--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/nico/NicoAudioSourceManager.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/nico/NicoAudioSourceManager.java
@@ -46,20 +46,24 @@ public class NicoAudioSourceManager implements AudioSourceManager, HttpConfigura
 
   private static final Pattern trackUrlPattern = Pattern.compile(TRACK_URL_REGEX);
 
-  private final String email;
-  private final String password;
   private final HttpInterfaceManager httpInterfaceManager;
   private final AtomicBoolean loggedIn;
 
+  public NicoAudioSourceManager() {
+    this(null, null);
+  }
+
   /**
-   * @param email Site account email
+   * @param email    Site account email
    * @param password Site account password
    */
   public NicoAudioSourceManager(String email, String password) {
-    this.email = email;
-    this.password = password;
     httpInterfaceManager = HttpClientTools.createDefaultThreadLocalManager();
     loggedIn = new AtomicBoolean();
+
+    if (!DataFormatTools.isNullOrEmpty(email) && !DataFormatTools.isNullOrEmpty(password)) {
+      login(email, password);
+    }
   }
 
   @Override
@@ -79,8 +83,6 @@ public class NicoAudioSourceManager implements AudioSourceManager, HttpConfigura
   }
 
   private AudioTrack loadTrack(String videoId) {
-    checkLoggedIn();
-
     try (HttpInterface httpInterface = getHttpInterface()) {
       try (CloseableHttpResponse response = httpInterface.execute(new HttpGet("http://ext.nicovideo.jp/api/getthumbinfo/" + videoId))) {
         int statusCode = response.getStatusLine().getStatusCode();
@@ -98,9 +100,9 @@ public class NicoAudioSourceManager implements AudioSourceManager, HttpConfigura
 
   private AudioTrack extractTrackFromXml(String videoId, Document document) {
     for (Element element : document.select(":root > thumb")) {
-      String uploader = element.select("user_nickname").first().text();
-      String title = element.select("title").first().text();
-      long duration = DataFormatTools.durationTextToMillis(element.select("length").first().text());
+      String uploader = element.selectFirst("user_nickname").text();
+      String title = element.selectFirst("title").text();
+      long duration = DataFormatTools.durationTextToMillis(element.selectFirst("length").text());
 
       return new NicoAudioTrack(new AudioTrackInfo(title, uploader, duration, videoId, false, getWatchUrl(videoId)), this);
     }
@@ -145,7 +147,7 @@ public class NicoAudioSourceManager implements AudioSourceManager, HttpConfigura
     httpInterfaceManager.configureBuilder(configurator);
   }
 
-  void checkLoggedIn() {
+  void login(String email, String password) {
     synchronized (loggedIn) {
       if (loggedIn.get()) {
         return;
--- main/src/main/java/com/sedmelluq/discord/lavaplayer/source/nico/NicoAudioTrack.java
+++ main/src/main/java/com/sedmelluq/discord/lavaplayer/source/nico/NicoAudioTrack.java
@@ -2,36 +2,46 @@ package com.sedmelluq.discord.lavaplayer.source.nico;
 
 import com.sedmelluq.discord.lavaplayer.container.mpeg.MpegAudioTrack;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.tools.DataFormatTools;
+import com.sedmelluq.discord.lavaplayer.tools.JsonBrowser;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpClientTools;
 import com.sedmelluq.discord.lavaplayer.tools.io.HttpInterface;
-import com.sedmelluq.discord.lavaplayer.tools.io.PersistentHttpStream;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrackInfo;
 import com.sedmelluq.discord.lavaplayer.track.DelegatedAudioTrack;
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
+import org.jsoup.parser.Parser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.sedmelluq.discord.lavaplayer.tools.DataFormatTools.convertToMapLayout;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Audio track that handles processing NicoNico tracks.
  */
 public class NicoAudioTrack extends DelegatedAudioTrack {
   private static final Logger log = LoggerFactory.getLogger(NicoAudioTrack.class);
+  private static final String JSON_STRING = "{\"session\":{\"content_type\":\"movie\",\"timing_constraint\":\"unlimited\",\"recipe_id\":\"%s\",\"content_id\":\"%s\",\"keep_method\":{\"heartbeat\":{\"lifetime\":%d}},\"content_src_id_sets\":[{\"content_src_ids\":[{\"src_id_to_mux\":{\"video_src_ids\":[\"%s\"],\"audio_src_ids\":[\"%s\"]}}]}],\"protocol\":{\"name\":\"http\",\"parameters\":{\"http_parameters\":{\"parameters\":{\"http_output_download_parameters\":{\"use_well_known_port\":\"%s\",\"use_ssl\":\"%s\"}}}}},\"session_operation_auth\":{\"session_operation_auth_by_signature\":{\"token\":\"%s\",\"signature\":\"%s\"}},\"content_auth\":{\"auth_type\":\"%s\",\"content_key_timeout\":%d,\"service_id\":\"nicovideo\",\"service_user_id\":\"%s\"},\"client_info\":{\"player_id\":\"%s\"}}}";
+
+  private static String actionTrackId = "S1G2fKdzOl_1702504390263";
 
   private final NicoAudioSourceManager sourceManager;
 
+  private String heartbeatUrl;
+  private int heartbeatIntervalMs;
+  private String initialHeartbeatPayload;
+
   /**
-   * @param trackInfo Track info
+   * @param trackInfo     Track info
    * @param sourceManager Source manager which was used to find this track
    */
   public NicoAudioTrack(AudioTrackInfo trackInfo, NicoAudioSourceManager sourceManager) {
@@ -42,47 +52,183 @@ public class NicoAudioTrack extends DelegatedAudioTrack {
 
   @Override
   public void process(LocalAudioTrackExecutor localExecutor) throws Exception {
-    sourceManager.checkLoggedIn();
-
     try (HttpInterface httpInterface = sourceManager.getHttpInterface()) {
-      loadVideoMainPage(httpInterface);
       String playbackUrl = loadPlaybackUrl(httpInterface);
 
       log.debug("Starting NicoNico track from URL: {}", playbackUrl);
 
-      try (PersistentHttpStream stream = new PersistentHttpStream(httpInterface, new URI(playbackUrl), null)) {
+      try (HeartbeatingHttpStream stream = new HeartbeatingHttpStream(
+          httpInterface,
+          new URI(playbackUrl),
+          null,
+          heartbeatUrl,
+          heartbeatIntervalMs,
+          initialHeartbeatPayload
+      )) {
         processDelegate(new MpegAudioTrack(trackInfo, stream), localExecutor);
       }
     }
   }
 
-  private void loadVideoMainPage(HttpInterface httpInterface) throws IOException {
-    HttpGet request = new HttpGet("http://www.nicovideo.jp/watch/" + trackInfo.identifier);
+  private JsonBrowser loadVideoApi(HttpInterface httpInterface) throws IOException {
+    String apiUrl = "https://www.nicovideo.jp/api/watch/v3_guest/" + getIdentifier() + "?_frontendId=6&_frontendVersion=0&actionTrackId=" + actionTrackId + "&i18nLanguage=en-us";
 
-    try (CloseableHttpResponse response = httpInterface.execute(request)) {
-      int statusCode = response.getStatusLine().getStatusCode();
-      if (!HttpClientTools.isSuccessWithContent(statusCode)) {
-        throw new IOException("Unexpected status code from video main page: " + statusCode);
-      }
+    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(apiUrl))) {
+      HttpClientTools.assertSuccessWithContent(response, "api response");
 
-      EntityUtils.consume(response.getEntity());
+      return JsonBrowser.parse(response.getEntity().getContent()).get("data");
+    }
+  }
+
+  private JsonBrowser loadVideoMainPage(HttpInterface httpInterface) throws IOException {
+    try (CloseableHttpResponse response = httpInterface.execute(new HttpGet(trackInfo.uri))) {
+      HttpClientTools.assertSuccessWithContent(response, "video main page");
+
+      String urlEncodedData = DataFormatTools.extractBetween(EntityUtils.toString(response.getEntity()), "data-api-data=\"", "\"");
+      String watchData = Parser.unescapeEntities(urlEncodedData, false);
+
+      return JsonBrowser.parse(watchData);
     }
   }
 
   private String loadPlaybackUrl(HttpInterface httpInterface) throws IOException {
-    HttpGet request = new HttpGet("http://flapi.nicovideo.jp/api/getflv/" + trackInfo.identifier);
+    JsonBrowser videoJson = loadVideoApi(httpInterface);
+
+    if (videoJson.isNull()) {
+      log.warn("Couldn't retrieve NicoNico video details from API, falling back to HTML page...");
+      videoJson = loadVideoMainPage(httpInterface);
+    }
+
+    if (!videoJson.isNull()) {
+      // an "actionTrackId" is necessary to receive an API response.
+      // We make sure this is kept up to date to prevent any issues with tracking IDs becoming invalid.
+      String trackingId = videoJson.get("client").get("watchTrackId").text();
+
+      if (trackingId != null) {
+        actionTrackId = trackingId;
+      }
+    }
+
+    String watchData = processJson(videoJson.get("media").get("delivery").get("movie").get("session"));
+
+    HttpPost request = new HttpPost("https://api.dmc.nico/api/sessions?_format=json");
+    request.addHeader("Host", "api.dmc.nico");
+    request.addHeader("Connection", "keep-alive");
+    request.addHeader("Content-Type", "application/json");
+    request.addHeader("Origin", "https://www.nicovideo.jp");
+    request.setEntity(new StringEntity(watchData));
 
     try (CloseableHttpResponse response = httpInterface.execute(request)) {
       int statusCode = response.getStatusLine().getStatusCode();
-      if (!HttpClientTools.isSuccessWithContent(statusCode)) {
+
+      if (statusCode != HttpStatus.SC_CREATED) {
         throw new IOException("Unexpected status code from playback parameters page: " + statusCode);
       }
 
-      String text = EntityUtils.toString(response.getEntity());
-      Map<String, String> format = convertToMapLayout(URLEncodedUtils.parse(text, StandardCharsets.UTF_8));
+      JsonBrowser info = JsonBrowser.parse(response.getEntity().getContent()).get("data");
+      JsonBrowser session = info.get("session");
 
-      return format.get("url");
+      heartbeatUrl = "https://api.dmc.nico/api/sessions/" + session.get("id").text() + "?_format=json&_method=PUT";
+      heartbeatIntervalMs = session.get("keep_method").get("heartbeat").get("lifetime").asInt(120000) - 5000;
+      initialHeartbeatPayload = info.format();
+
+      return session.get("content_uri").text();
     }
+  }
+
+  private String processJson(JsonBrowser input) {
+    if (input.isNull()) {
+      throw new IllegalStateException("Invalid response received from NicoNico when loading video details");
+    }
+
+    List<String> videos = input.get("videos").values().stream()
+        .map(JsonBrowser::text)
+        .collect(Collectors.toList());
+
+    List<String> audios = input.get("audios").values().stream()
+        .map(JsonBrowser::text)
+        .collect(Collectors.toList());
+
+    JsonBrowser url = input.get("urls").index(0);
+    boolean useWellKnownPort = url.get("isWellKnownPort").asBoolean(false);
+    boolean useSsl = url.get("isSsl").asBoolean(false);
+
+    // Honestly this looks awful, and it's probably super unfriendly to work with should the JSON structure change in future
+    // buuuuuuuuuuuuuuut this method only takes ~1ms to process. Constructing a JSON object properly (the commented code below this)
+    // takes around 5ms. In the grand scheme of things, saving 4ms at the expense of maintainability is not worth it but this is funny
+    // ...and I hope this doesn't need to be changed frequently in future. If it does, I'll just use the method below.
+    return String.format(
+        JSON_STRING,
+        input.get("recipeId").text(),
+        input.get("contentId").text(),
+        input.get("heartbeatLifetime").asLong(120000),
+        String.join("\",\"", videos),
+        String.join("\",\"", audios),
+        useWellKnownPort ? "yes" : "no",
+        useSsl ? "yes" : "no",
+        input.get("token").text().replace("\"", "\\\""),
+        input.get("signature").text(),
+        input.get("authTypes").get("http").text(),
+        input.get("contentKeyTimeout").asLong(120000),
+        input.get("serviceUserId").text(),
+        input.get("playerId").text()
+    );
+
+//    return JsonWriter.string()
+//        .object()
+//          .object("session")
+//            .value("content_type", "movie")
+//            .value("timing_constraint", "unlimited")
+//            .value("recipe_id", input.get("recipeId").text())
+//            .value("content_id", input.get("contentId").text())
+//            .object("keep_method")
+//              .object("heartbeat")
+//                .value("lifetime", input.get("heartbeatLifetime").asLong(120000))
+//              .end()
+//            .end()
+//            .array("content_src_id_sets")
+//              .object()
+//                .array("content_src_ids")
+//                  .object()
+//                    .object("src_id_to_mux")
+//                      .array("video_src_ids", videos)
+//                      .array("audio_src_ids", audios)
+//                    .end()
+//                  .end()
+//                .end()
+//              .end()
+//            .end()
+//            .object("protocol")
+//              .value("name", "http")
+//              .object("parameters")
+//                .object("http_parameters")
+//                  .object("parameters")
+//                    .object("http_output_download_parameters")
+//                      .value("use_well_known_port", useWellKnownPort ? "yes" : "no")
+//                      .value("use_ssl", useSsl ? "yes" : "no")
+//                    .end()
+//                  .end()
+//                .end()
+//              .end()
+//            .end()
+//            .object("session_operation_auth")
+//              .object("session_operation_auth_by_signature")
+//                .value("token", input.get("token").text())
+//                .value("signature", input.get("signature").text())
+//              .end()
+//            .end()
+//            .object("content_auth")
+//              .value("auth_type", input.get("authTypes").get("http").text())
+//              .value("content_key_timeout", input.get("contentKeyTimeout").asLong(120000))
+//              .value("service_id", "nicovideo")
+//              .value("service_user_id", input.get("serviceUserId").text())
+//            .end()
+//            .object("client_info")
+//              .value("player_id", input.get("playerId").text())
+//            .end()
+//          .end()
+//        .end()
+//        .done();
   }
 
   @Override
